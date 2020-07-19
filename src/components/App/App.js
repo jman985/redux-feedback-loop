050d1a7b876199d0e4e8d6151cdbaab26d0328a1
Josh Maus <jman985@gmail.com>
2020-07-18 19:30:02 -0500

    added header and components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
+      <Header />
         <br/>
+        <button>Begin</button>
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+
+    render() {
+        return (
+        
+            <header className="header">
+              <h1 className="title">Feedback!</h1>
+              <h4><i>Don't forget it!</i></h4>
+            </header>
+    
+        )
+      }
+}
+
+export default Header;
